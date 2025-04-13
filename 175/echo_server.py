c06d1ea07ab0bf8b416bdc6ef21eeecd53fe9a33
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-13 15:05:04 -0700

    Add server socket accepting incoming traffic and continuing to listen

--- 175/echo_server.py
+++ 175/echo_server.py
@@ -3,3 +3,10 @@ import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 3003))
 server_socket.listen()
+print('Server is running on localhost:3003')
+
+while True:
+    client_socket, addr = server_socket.accept()
+    print(f'Connection from {addr}')
+    request = client_socket.recv(1024).decode()
+
