7db1879cc1317d8482310df8810ab14eed7020b5
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-13 17:41:45 -0700

    Add attempt at roll dice implementation with number of rolls and sides chosen

--- 175/roll_dice.py
+++ 175/roll_dice.py
@@ -1,6 +1,14 @@
 import socket
 import random
 
+def parse_path(unparsed_path):
+    path, parameter_string = unparsed_path.split('?')
+    rolls, sides = parameter_string.split('&')
+    _, rolls = rolls.split('=')
+    _, sides = sides.split('=')
+    query_parameters = { 'rolls': int(rolls), 'sides': int(sides) }
+    return path, query_parameters
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 3003))
 server_socket.listen()
@@ -15,9 +23,23 @@ while True:
         continue
 
     request_line = request.splitlines()[0]
+    request_line_components = request_line.split(' ')
+    request_HTTP_method = request_line_components[0]
+    unparsed_path = request_line_components[2]
+    path, query_parameters = parse_path(unparsed_path)
 
-    roll = random.randint(1, 6)
-    response_body = f"{request_line}\n{roll}\n"
+    response_body = (
+        f"Request: {request_line}\n"
+        f"HTTP Method: {request_HTTP_method}\n"
+        f"Path: {request_path}\n"
+        f"Parameters: {query_parameters}\n"
+    )
+
+    rolls = 0
+    while rolls < query_parameters['roll']:
+        roll = random.randint(1, query_parameters['sides'])
+        response_body += f"{roll}\n"
+        rolls += 1
 
     response = ("HTTP/1.1 200 OK\r\n"
                 "Content-Type: text/plains\r\n"
