8ee749920edbecadba02e1c6fc54c2fb91d85e69
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-19 19:41:14 -0400

    Change chapter loading to load lines instead of all text

--- 175/book_viewer_starter/app.py
+++ 175/book_viewer_starter/app.py
@@ -9,14 +9,14 @@ def index():
     return render_template('index.html', contents=contents)
 
 @app.route("/chapters/1")
-def index():
+def chapter():
     with open("book_viewer/data/toc.txt", "r") as file:
         contents = file.readlines()
     with open("book_viewer/data/chp1.txt") as file:
-        chapter = file.read()
+        chapter = file.readlines()
     return render_template('chapter.html',
                             contents=contents,
-                            chapter=chp)
+                            chapter=chapter)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
