6ce21ea5cc84b589f1f313912e146d103386552a
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-19 19:36:55 -0400

    Add chapter 1 route

--- 175/book_viewer_starter/app.py
+++ 175/book_viewer_starter/app.py
@@ -8,5 +8,15 @@ def index():
         contents = file.readlines()
     return render_template('index.html', contents=contents)
 
+@app.route("/chapters/1")
+def index():
+    with open("book_viewer/data/toc.txt", "r") as file:
+        contents = file.readlines()
+    with open("book_viewer/data/chp1.txt") as file:
+        chapter = file.read()
+    return render_template('chapter.html',
+                            contents=contents,
+                            chapter=chp)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
