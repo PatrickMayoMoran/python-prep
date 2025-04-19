0f97ae5877b6d122280cbb556815898d0fb90d4f
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-19 12:34:03 -0400

    Add table of contents chapter listing to index template

--- 175/book_viewer_starter/app.py
+++ 175/book_viewer_starter/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    with open("book_viewer/data/toc.txt", "r") as file:
+        contents = file.readlines()
+    return render_template('index.html', contents=contents)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
