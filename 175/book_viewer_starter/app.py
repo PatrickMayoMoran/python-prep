b324ca31747e8588c106a7fe571e91fbe717980d
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-21 10:55:38 -0400

    Add before_request block for loading table of contents

--- 175/book_viewer_starter/app.py
+++ 175/book_viewer_starter/app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
+@app.before_request
+def load_contents():
+    with open("book_viewer/data/toc.txt", "r") as file:
+        g.contents = file.readlines()
+
 @app.route("/")
 def index():
-    with open("book_viewer/data/toc.txt", "r") as file:
-        contents = file.readlines()
-    return render_template('home.html', contents=contents)
+    return render_template('home.html', contents=g.contents)
 
 @app.route("/chapters/<page_num>")
 def chapter(page_num):
-    with open("book_viewer/data/toc.txt", "r") as file:
-        contents = file.readlines()
-
-    chapter_name = contents[int(page_num) - 1]
+    chapter_name = g.contents[int(page_num) - 1]
     chapter_title = f"Chapter {page_num}: {chapter_name}"
 
     with open(f"book_viewer/data/chp{page_num}.txt") as file:
@@ -21,7 +21,7 @@ def chapter(page_num):
         chapter = chapter.split("\n\n")
     return render_template('chapter.html',
                             chapter_title=chapter_title,
-                            contents=contents,
+                            contents=g.contents,
                             chapter=chapter)
 
 if __name__ == "__main__":
