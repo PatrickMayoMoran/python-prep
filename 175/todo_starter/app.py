123160a6be9462d28542026ffdc0bb14f87dbd7f
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-22 12:59:49 -0400

    Add starting list display data to lists page

--- 175/todo_starter/app.py
+++ 175/todo_starter/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("lists.html")
+    lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
+
+    return render_template("lists.html", lists=lists)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
