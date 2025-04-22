f309d4107788a79606b1633a96f66cf2a137ab4c
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-22 13:21:35 -0400

    Add lists display page

--- 175/todo_starter/app.py
+++ 175/todo_starter/app.py
@@ -15,5 +15,14 @@ def index():
 def add_todo_list():
     return render_template("new_list.html")
 
+# Render the list of todo lists
+@app.route("/lists")
+def get_lists():
+    lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
+    return render_template('lists.html', lists=lists)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
