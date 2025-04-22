7a301c0dd2115643fd37be54ceda5a9e33521a13
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-22 13:35:11 -0400

    Change home route to redirect to lists page

--- 175/todo_starter/app.py
+++ 175/todo_starter/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    lists = [
-        {"title": "Lunch Groceries", "todos": []},
-        {"title": "Dinner Groceries", "todos": []},
-    ]
-
-    return render_template("lists.html", lists=lists)
+    return redirect(url_for('get_lists'))
 
 @app.route("/lists/new")
 def add_todo_list():
