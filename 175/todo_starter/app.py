2dfb58e9e6ab8d53f0454980c2aa5061ad874fdf
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-25 11:51:12 -0400

    Add route for editing single list name

--- 175/todo_starter/app.py
+++ 175/todo_starter/app.py
@@ -28,6 +28,7 @@ def initialize_session():
     if 'lists' not in session:
         session['lists'] = []
 
+# Redirect to view all lists
 @app.route("/")
 def index():
     return redirect(url_for('get_lists'))
@@ -41,6 +42,7 @@ def add_todo_list():
 def get_lists():
     return render_template('lists.html', lists=session['lists'])
 
+# Add new list
 @app.route("/lists", methods=["POST"])
 def create_list():
     title = request.form["list_title"].strip()
@@ -60,6 +62,7 @@ def create_list():
     session.modified = True
     return redirect(url_for('get_lists'))
 
+# Show single list
 @app.route("/lists/<list_id>")
 def show_list(list_id):
     lst = find_list_by_id(list_id, session['lists'])
@@ -68,6 +71,7 @@ def show_list(list_id):
 
     return render_template('list.html', lst=lst)
 
+# Add todo to a list
 @app.route("/lists/<list_id>/todos", methods=["POST"])
 def add_todo(list_id):
     title = request.form['todo'].strip()
@@ -90,6 +94,7 @@ def add_todo(list_id):
     session.modified = True
     return redirect(url_for('show_list', list_id=list_id))
 
+# Complete or incomplete single todo
 @app.route('/lists/<list_id>/todos/<todo_id>/toggle', methods=["POST"])
 def toggle(list_id, todo_id):
     lst = find_list_by_id(list_id, session['lists'])
@@ -112,6 +117,7 @@ def toggle(list_id, todo_id):
     session.modified = True
     return redirect(url_for('show_list', list_id=list_id))
 
+# Delete a todo
 @app.route("/lists/<list_id>/todos/<todo_id>/delete", methods=["POST"])
 def delete_todo(list_id, todo_id):
     lst = find_list_by_id(list_id, session['lists'])
@@ -127,6 +133,7 @@ def delete_todo(list_id, todo_id):
     session.modified = True
     return redirect(url_for('show_list', list_id=list_id))
 
+# Complete all todos
 @app.route("/lists/<list_id>/complete_all", methods=["POST"])
 def complete_all(list_id):
     lst = find_list_by_id(list_id, session['lists'])
@@ -138,6 +145,12 @@ def complete_all(list_id):
     flash("All todos have been completed.", "success")
     session.modified = True
     return redirect(url_for('show_list', list_id=list_id))
+
+@app.route("/lists/<list_id>", methods=["POST"])
+def edit_list(list_id):
+    lst = find_list_by_id(list_id, session["lists"])
+    if not lst:
+        raise NotFound(description="List not found")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
